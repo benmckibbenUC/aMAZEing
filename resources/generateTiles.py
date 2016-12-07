c9c7d492b91bcf2d312690c7924d1e20dedfcf1b
Yan Verdeja Herms <yanverdeja@gmail.com>
2016-12-06 20:44:05 -0600

    fix up tile stl generator

--- resources/generateTiles.py
+++ resources/generateTiles.py
@@ -4,28 +4,30 @@
 # aMAZEing
 
 import sys
-from os import path, system
+from os import path, system, makedirs
 
 class tileGenerator():
     def __init__(self, tileWidth):
         head, _ = path.split(path.abspath(sys.argv[0]))
         self.tileWidth = tileWidth
         self.directory = path.join(head, str(tileWidth))
+        if not path.exists(self.directory):
+            makedirs(self.directory)
+        headerFile = open(path.join(head, 'scadHeaderTile.txt'), 'r')
+        self.header = headerFile.read(-1)
+        headerFile.close()
 
     def makeTileStls(self):
-        scadHeaderFile = open(path.join(self.directory, 'scadHeaderTile.txt'), 'r')
-        scadHeader = scadHeaderFile.read(-1)
-        scadHeaderFile.close()
         for i in range(0, 16):
             u = str(bool(i & 8))
             d = str(bool(i & 4))
             l = str(bool(i & 2))
             r = str(bool(i & 1))
-            scadFilename = path.join(self.directory, self.tileWidth + '.scad')
-            stlFilename = path.join(self.directory, self.tileWidth + '.stl')
+            scadFilename = path.join(self.directory, str(i) + '.scad')
+            stlFilename = path.join(self.directory, str(i) + '.stl')
             scadFile = open(scadFilename, 'w')
-            scadFile.write(scadHeader)
-            scadFile.write('tile(' + str(self.tileWidth) + ', ' + u + ', ' + d + ', ' + l + ', ' + r ')\n')
+            scadFile.write(self.header)
+            scadFile.write('tile(' + str(self.tileWidth) + ', ' + u + ', ' + d + ', ' + l + ', ' + r + ')\n')
             scadFile.close()
 
             print('openscad -o ' + stlFilename + ' ' + scadFilename)
@@ -35,4 +37,4 @@ class tileGenerator():
 
 if __name__ == '__main__':
     generator = tileGenerator(8)
-    generator.writeSCAD()
+    generator.makeTileStls()
