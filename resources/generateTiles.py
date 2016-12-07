578c1a11e84fe53165695cad8e2ac7fde45a8de0
Yan Verdeja Herms <yanverdeja@gmail.com>
2016-12-06 21:22:01 -0600

    add all marble sizes

--- resources/generateTiles.py
+++ resources/generateTiles.py
@@ -10,7 +10,7 @@ class tileGenerator():
     def __init__(self, tileWidth):
         head, _ = path.split(path.abspath(sys.argv[0]))
         self.tileWidth = tileWidth
-        self.directory = path.join(head, str(tileWidth))
+        self.directory = path.join(head, 'tileSTL/' + str(tileWidth))
         if not path.exists(self.directory):
             makedirs(self.directory)
         headerFile = open(path.join(head, 'scadHeaderTile.txt'), 'r')
@@ -27,14 +27,15 @@ class tileGenerator():
             stlFilename = path.join(self.directory, str(i) + '.stl')
             scadFile = open(scadFilename, 'w')
             scadFile.write(self.header)
-            scadFile.write('tile(' + str(self.tileWidth) + ', ' + u + ', ' + d + ', ' + l + ', ' + r + ')\n')
+            scadFile.write('\ntile(' + str(self.tileWidth) + ', ' + u + ', ' + d + ', ' + l + ', ' + r + ');\n')
             scadFile.close()
 
-            print('openscad -o ' + stlFilename + ' ' + scadFilename)
-        print('rm ' + self.directory + '*.scad')
+            system('openscad -o ' + stlFilename + ' ' + scadFilename)
+        system('rm ' + self.directory + '/*.scad')
 
 
 
 if __name__ == '__main__':
-    generator = tileGenerator(8)
-    generator.makeTileStls()
+    for i in range(0, 26):
+        generator = tileGenerator(i)
+        generator.makeTileStls()
